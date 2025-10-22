d1a9e1d26c7b41244c72bcb1ec203ee227bc9d1d
Корниенко Савелий Юрьевич <10230289@college.local>
2025-10-22 10:31:42 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,27 @@
+
+fun main() {
+    // Список городов
+    val cities = listOf(
+        "moskva",
+        "voronesh",
+        "spb",
+        "novosib",
+        "ekb",
+        "kazan",
+        "nizniy novgorod",
+        "chelabinsk",
+        "samara",
+        "tomsk",
+        "omsk",
+        "ufa",
+        "krasnodar",
+        "perm",
+        "vologda",
+        "petrozavodsk"
+    )
+    var running = true
+    while (running) {
+        println("\nВыберите действие:")
+        println("1. Создать маршрут поезда")
+        println("2. Выйти из программы")
+}
